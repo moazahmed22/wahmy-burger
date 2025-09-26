f99b7114ef6ff5e5ad07a5b87b53baf221453edc
moazahmed22 <nabelnadel21@gmail.com>
2025-09-26 03:55:38 +0300

    fix middleware

--- src/app/(pages)/wishlist/page.tsx
+++ src/app/(pages)/wishlist/page.tsx
@@ -5,6 +5,7 @@ import React, { useEffect } from "react";
 
 const Wishlist = () => {
   const { wishlistProducts, getWishlistDetails } = useWishlist();
+  
 
   useEffect(() => {
     getWishlistDetails();
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("next-auth.session-token");
+export async function middleware(request: NextRequest) {
+  // get the session cookie
+  const token =
+    request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("__Secure-next-auth.session-token")?.value;
 
   if (token) {
     return NextResponse.next();
@@ -12,7 +14,6 @@ export function middleware(request: NextRequest) {
   }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/profile", "/wishlist"],
+  matcher: ["/wishlist"],
 };
